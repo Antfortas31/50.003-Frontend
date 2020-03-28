11b5d569be8caddd513e47928dd5c8cc9129c728
Antfortas <ngjoshen@gmail.com>
2020-03-28 14:29:21 +0800

    Updates

---
+++ precache-manifest.11ed54bac875c0db1d4cf962412e74b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1a6ad1b7b5bee184cfc946ccf00c5f",
+    "url": "/50.003-Frontend/index.html"
+  },
+  {
+    "revision": "59eb819596681d769690",
+    "url": "/50.003-Frontend/static/css/main.eb647723.chunk.css"
+  },
+  {
+    "revision": "4326e30ffe4a0be7d14e",
+    "url": "/50.003-Frontend/static/js/2.5db83b46.chunk.js"
+  },
+  {
+    "revision": "9a61a502dd94b7ef55c6d2eae01aacc4",
+    "url": "/50.003-Frontend/static/js/2.5db83b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59eb819596681d769690",
+    "url": "/50.003-Frontend/static/js/main.ca781026.chunk.js"
+  },
+  {
+    "revision": "636fb8fe44ce3a13fd33",
+    "url": "/50.003-Frontend/static/js/runtime-main.5e9c565e.js"
+  },
+  {
+    "revision": "36437e89dbc4ce05f44e43a77ccbb5ac",
+    "url": "/50.003-Frontend/static/media/floorplan.36437e89.png"
+  }
+]);
